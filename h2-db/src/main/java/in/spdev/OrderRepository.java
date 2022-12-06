857e06b79a527a00db6e7c4bc71f88d147eca0db
techAnshumaan <tech.spmaan@gmail.com>
2022-12-06 12:12:38 +0530

    resource-update

--- h2-db/src/main/java/in/spdev/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package in.spdev;
-
-import in.spdev.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order,Integer> {
-}
